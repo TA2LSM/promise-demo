fa6f35e457fe7fd00466a9aaccb4bb90a0ce721c
TA2LSM <semihsenol@gmail.com>
2022-03-02 18:02:12 +0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//--- 02.03.2022 ---
+// --- 02.03.2022 ---
 
 console.log("Before");
 
@@ -18,16 +18,35 @@ p.then((user) => console.log(user));
 //getUser(1).then((user) => console.log(user)); // şeklinde de yazılabilir. (kullanılan bu)
 */
 
-getUser(1)
-  .then((user) =>
-    getRepositories(user.gitHubUserName).then((repositories) =>
-      getCommits(repositories[0]).then((commits) => console.log("All commits:", commits))
-    )
-  )
-  .catch((err) => console.log("Error:", err.message));
-// buradaki catch yukarıdaki async işlemlerden herhangi birinde sıkıntı olursa çalışacak.
+// Promise temelli yaklaşımla yazılmış kod
+// getUser(1)
+//   .then((user) =>
+//     getRepositories(user.gitHubUserName).then((repositories) =>
+//       getCommits(repositories[0]).then((commits) => console.log("All commits:", commits))
+//     )
+//   )
+//   .catch((err) => console.log("Error:", err.message));
+// // buradaki catch yukarıdaki async işlemlerden herhangi birinde sıkıntı olursa çalışacak.
 
+// Await temelli yaklaşımla yazılmış kod
+// Async ve Await promise çatısına inşa edilmiştir.
+// Await ile yine resolve beklenir ve aşağıdaki kod bir şekilde yukarıdakine çevrilerek
+// işletilir. Await kısmındaki hataları yakalamak içinse try-catch kullanmamız lazım.
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUserName);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
+}
+
+displayCommits();
 console.log("After");
+// kod işlediği zaman console.log işlemleri yapılır diğer kısımlar ise teker teker işler
+// ve sonuçlanır.
 
 //callback default bir isimdir ve tanınımlıdır. Bu çekilde yazılması gerekiyor.
 function getUser(id) {
@@ -44,6 +63,7 @@ function getRepositories(username) {
     setTimeout(() => {
       console.log("Reading user's repositories form GitHub...");
       resolve(["repo1", "repo2", "repo3"]);
+      //reject(new Error("Could not get the repositories!"));
     }, 2000);
   });
 }
